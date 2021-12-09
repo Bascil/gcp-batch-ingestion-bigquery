c2fe59d9151810d6d883bf703a4b1ff5eae53648
Basil Ndonga <basilndonga@gmail.com>
2021-12-09 07:28:58 +0300

    first commit

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -10,7 +10,7 @@ provider "google" {
   region = "australia-southeast1-a"
 }
 
-resource "google_storage_bucket" "artisto-funky-bucket" {
+resource "google_storage_bucket" "funky-bucket" {
   name = "test-batch-pipeline"
   storage_class = "REGIONAL"
   location  = "australia-southeast1"
